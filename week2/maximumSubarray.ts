298b499e3dac075153dc17c38332f452d0470041
alexanderkohzm <127705458+alexanderkohzm@users.noreply.github.com>
2023-03-26 23:27:11 +0800

    Max Sub array works - should use Kaldane's algo

--- week2/maximumSubarray.ts
+++ week2/maximumSubarray.ts
@@ -8,84 +8,72 @@
 // Output = 6
 // Explanation - the subarray [4, -1, 2, 1] has the largest sum 6 
 
+// there are two solutions to this
+// you can check the sum of index 0 till end, index 1 till end, index 2 till end etc
+// but this is O(n^2) time 
+
+// the optimal solution is Kadane's algorithm 
+
 function maxSubArray(nums: number[]): number {
 
   // early return if length is 1
   if (nums.length === 1) {
     return nums[0]
   }
-  let currentLargestSubArray = [nums[0]]
 
-  let nextSubArray = [] as Array<number>
+  let maxSum = nums[0]
 
-  for (let i = 1; i < nums.length; i++) {
 
-    const number = nums[i]
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0
 
-    const currentLargestSum = currentLargestSubArray.reduce((prev, curr) => {
-      // console.log(`prev: ${prev}, curr: ${curr}`)
-      return prev + curr
-    }, 0)
+    for (let j = i; j < nums.length; j++) {
+      sum += nums[j]
 
-    const nextSubArraySum = nextSubArray.reduce((prev, curr) => {
-      // console.log(`prev: ${prev}, curr: ${curr}`)
-      return prev + curr
-    }, 0)
-
-    const contenderSum = currentLargestSum + number
-    const nextSubArrayAndCurr = nextSubArraySum + number
-
-    console.log(`ContenderSum: ${contenderSum}, nextSub: ${nextSubArrayAndCurr}`)
-
-    if (currentLargestSum < contenderSum && contenderSum > nextSubArrayAndCurr) {
-      currentLargestSubArray.push(number)
-    } else if (currentLargestSum > contenderSum) {
-      currentLargestSubArray.push(number)
-      nextSubArray.push(number)
-    } else if (nextSubArrayAndCurr > currentLargestSum) {
-      currentLargestSubArray = nextSubArray
-      nextSubArray = []
+      if (sum > maxSum) {
+        maxSum = sum
+      }
     }
   }
-  return currentLargestSubArray.reduce((prev, curr) => {
-    return prev + curr
-  }, 0)
+
+  return maxSum
 }
 
-// for (const number of nums) {
+// O(n) time complexity 
+function maxSubArrayKadaneAlgo(nums: number[]): number {
 
-//   // reduce the currentLargestSubArray 
-//   const currentLargestSum = currentLargestSubArray.reduce((prev, curr) => {
-//     console.log(`prev: ${prev}, curr: ${curr}`)
-//     return prev + curr
-//   }, 0)
 
-//   const nextSubArraySum = nextSubArray.reduce((prev, curr) => {
-//     console.log(`prev: ${prev}, curr: ${curr}`)
-//     return prev + curr
-//   }, 0)
+  // let maxSum = nums[0]
+  // let currentSum = maxSum
 
-//   console.log(`CurrentLargestSum: ${currentLargestSum}, nextSubArraySum: ${nextSubArraySum}`)
+  // for (let i = 1; i < nums.length; i++) {
+  //   currentSum = Math.max(nums[i] + currentSum, nums[i])
+  //   maxSum = Math.max(currentSum, maxSum)
+  // }
+  // return maxSum
 
-//   const contenderSum = currentLargestSum + number
-//   const nextSubArrayAndCurr = nextSubArraySum + number
+  let maxSum = nums[0]
+  let currentSum = 0
 
-//   console.log(`ContenderSum: ${contenderSum}, nextSub: ${nextSubArrayAndCurr}`)
-
-//   if (currentLargestSum < contenderSum && contenderSum > nextSubArrayAndCurr) {
-//     currentLargestSubArray.push(number)
-//   } else if (currentLargestSum > contenderSum) {
-//     nextSubArray.push(number)
-//   } else if (nextSubArrayAndCurr > currentLargestSum) {
-//     currentLargestSubArray = nextSubArray
-//   }
-// }
+  for (let i = 0; i < nums.length; i++) {
+    if (currentSum < 0) {
+      currentSum = 0
+    }
+    currentSum += nums[i]
+    maxSum = Math.max(maxSum, currentSum)
+  }
+  return maxSum
+}
 
 
 
-// const nums23123 = [5, 4, -1, 7, 8]
+const nums23123 = [5, 4, -1, 7, 8]
 
 const nums213912903 = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 
 // console.log(maxSubArray(nums23123))
-console.log(maxSubArray(nums213912903))
+// console.log(maxSubArray(nums213912903))
+
+console.log(maxSubArrayKadaneAlgo(nums23123))
+
+console.log(maxSubArrayKadaneAlgo(nums213912903))
